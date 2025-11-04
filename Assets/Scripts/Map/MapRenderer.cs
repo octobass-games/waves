fc49d7afaacaae7cd525de68ed3376e0c8f67744
Megan <megan@theparkes.me.uk>
2025-11-04 12:47:58 +0000

    add map prefab

--- Assets/Scripts/Map/MapRenderer.cs
+++ Assets/Scripts/Map/MapRenderer.cs
@@ -7,5 +7,26 @@ namespace Octobass.Waves.Map
     {
         public Cartographer Cartographer;
         public List<MapRoomRenderer> RoomRenderers;
+
+        public bool miniMode = true;
+
+        public GameObject MiniMap;
+        public GameObject BigMap;
+
+        public void ToggleMode()
+        {
+            miniMode = !miniMode;
+
+            if (miniMode)
+            {
+                MiniMap.SetActive(true);
+                BigMap.SetActive(false);
+            }
+            else
+            {
+                MiniMap.SetActive(false);
+                BigMap.SetActive(true);
+            }
+        }
     }
 }
