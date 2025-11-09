f56dc0b0d9bac3e03500551884bd9b18e3afeb2c
adamskipadamski <adam.t.wright@outlook.com>
2025-11-09 00:18:11 +0000

    use AnimationController in CharacterBrain

--- Assets/Scripts/Character/AnimationController.cs
+++ Assets/Scripts/Character/AnimationController.cs
@@ -4,5 +4,60 @@ namespace Octobass.Waves.Character
 {
     public class AnimationController : MonoBehaviour
     {
+        public Animator Animator;
+        public SpriteRenderer SpriteRenderer;
+
+        private AttackStateId PreviousAttackState;
+        private AttackStateId CurrentAttackState;
+        private MovementSnapshot CurrentMovementSnapshot = new(CharacterStateId.Grounded, Vector2.zero, Vector2.right);
+        private MovementSnapshot PreviousMovementSnapshot = new(CharacterStateId.Grounded, Vector2.zero, Vector2.right);
+
+        public void Tick(MovementSnapshot movementSnapshot, AttackStateId attackState)
+        {
+            PreviousMovementSnapshot = CurrentMovementSnapshot;
+            CurrentMovementSnapshot = movementSnapshot;
+
+            PreviousAttackState = CurrentAttackState;
+            CurrentAttackState = attackState;
+
+            if (PreviousMovementSnapshot.State != CurrentMovementSnapshot.State)
+            {
+                switch (CurrentMovementSnapshot.State)
+                {
+                    case CharacterStateId.Grounded:
+                        Animator.SetTrigger("IsGrounded");
+                        break;
+                    case CharacterStateId.Jumping:
+                        Animator.SetTrigger("Jump");
+                        break;
+                    case CharacterStateId.Falling:
+                        Animator.SetTrigger("Fall");
+                        break;
+                    case CharacterStateId.WallClimb:
+                        Animator.SetTrigger("WallClimb");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (PreviousAttackState != CurrentAttackState)
+            {
+                switch (CurrentAttackState)
+                {
+                    case AttackStateId.Attacking:
+                        Animator.SetTrigger("MeleeAttack");
+                        break;
+                    default:
+                        break;
+                }
+
+                PreviousAttackState = CurrentAttackState;
+            }
+
+            Animator.SetBool("HasXVelocity", CurrentMovementSnapshot.Displacement.x != 0);
+            Animator.SetBool("HasYVelocity", CurrentMovementSnapshot.Displacement.y != 0);
+            SpriteRenderer.flipX = CurrentMovementSnapshot.FacingDirection == Vector2.left;
+        }
     }
 }
--- Assets/Scripts/Character/CharacterBrain.cs
+++ Assets/Scripts/Character/CharacterBrain.cs
@@ -5,95 +5,34 @@ namespace Octobass.Waves.Character
 {
     public class CharacterBrain : MonoBehaviour
     {
-        public MovementStateMachine MovementController;
+        public MovementStateMachine MovementStateMachine;
         public AttackController AttackController;
-
-        public Rigidbody2D Body;
-        public MovementConfig CharacterControllerConfig;
-        public Animator Animator;
-        public SpriteRenderer SpriteRenderer;
+        public AnimationController AnimationController;
 
         public CharacterController2DDriver Driver;
         private CharacterController2DDriverSnapshot Snapshot = new();
 
-        private bool AnimatorUpdated;
-        private bool AttackAnimationUpdated;
-        private Vector2 Displacement;
-
-        private MovementControllerCollisionDetector CollisionDetector;
-
-        private AttackStateId PreviousAttackState;
-        private AttackStateId CurrentAttackState;
-
-        private MovementSnapshot CurrentMovementSnapshot = new(CharacterStateId.Grounded, Vector2.zero, Vector2.right);
-        private MovementSnapshot PreviousMovementSnapshot = new(CharacterStateId.Grounded, Vector2.zero, Vector2.right);
-
-        void Awake()
-        {
-            CollisionDetector = new MovementControllerCollisionDetector(Body, CharacterControllerConfig);
-        }
+        private MovementSnapshot MovementSnapshot = new(CharacterStateId.Grounded, Vector2.zero, Vector2.right);
 
         void Update()
         {
             Snapshot = Driver.TakeSnapshot();
 
-            CurrentAttackState = AttackController.GetCurrentState();
-
-            if (PreviousMovementSnapshot.State != CurrentMovementSnapshot.State)
-            {
-                switch (CurrentMovementSnapshot.State)
-                {
-                    case CharacterStateId.Grounded:
-                        Animator.SetTrigger("IsGrounded");
-                        break;
-                    case CharacterStateId.Jumping:
-                        Animator.SetTrigger("Jump");
-                        break;
-                    case CharacterStateId.Falling:
-                        Animator.SetTrigger("Fall");
-                        break;
-                    case CharacterStateId.WallClimb:
-                        Animator.SetTrigger("WallClimb");
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            PreviousMovementSnapshot = CurrentMovementSnapshot;
-
-            if (PreviousAttackState != CurrentAttackState)
-            {
-                switch (CurrentAttackState)
-                {
-                    case AttackStateId.Attacking:
-                        Debug.Log($"Setting melee trigger - {CurrentAttackState}");
-                        Animator.SetTrigger("MeleeAttack");
-                        break;
-                    default:
-                        break;
-                }
-
-                PreviousAttackState = CurrentAttackState;
-            }
-
-            Animator.SetBool("HasXVelocity", CurrentMovementSnapshot.Displacement.x != 0);
-            Animator.SetBool("HasYVelocity", CurrentMovementSnapshot.Displacement.y != 0);
-            SpriteRenderer.flipX = CurrentMovementSnapshot.FacingDirection == Vector2.left;
+            AnimationController.Tick(MovementSnapshot, AttackController.GetCurrentState());
         }
 
         void FixedUpdate()
         {
             AttackController.Tick(Snapshot);
 
-            CurrentMovementSnapshot = MovementController.Tick(Snapshot);
+            MovementSnapshot = MovementStateMachine.Tick(Snapshot);
 
             Driver.Consume();
         }
 
         public void OnAbilityItemPickedUp(AbilityItemInstance ability)
         {
-            MovementController.AddState(ability.NewState);
+            MovementStateMachine.AddState(ability.NewState);
         }
     }
 }
