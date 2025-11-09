5eaa5bcb8b0beca6e8936574de6ccc6dcfdfb351
Megan <megan@theparkes.me.uk>
2025-11-09 12:31:30 +0000

    update fade

--- Assets/Scripts/OpeningAnimation.cs
+++ Assets/Scripts/OpeningAnimation.cs
@@ -1,10 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class OpeningAnimation : MonoBehaviour
 {
-    void openingEnd()
+    public GameObject FadeCanvas;
+    public void openingEnd()
     {
+        FadeCanvas.SetActive(true);
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(3);
+
         SceneManager.LoadScene("Game");
     }
 }
