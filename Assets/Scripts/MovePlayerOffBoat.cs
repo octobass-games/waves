61f4a43bc64a4f6a7ede0c82bea5bc4f61405c49
adamskipadamski <adam.t.wright@outlook.com>
2025-11-15 10:42:03 +0000

    hide mini-map while riding boat

--- Assets/Scripts/MovePlayerOffBoat.cs
+++ Assets/Scripts/MovePlayerOffBoat.cs
@@ -1,22 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class MovePlayerOffBoat : MonoBehaviour
+namespace Octobass.Waves
 {
-    public GameObject Player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class MovePlayerOffBoat : MonoBehaviour
     {
-        
-    }
+        public GameObject Player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        public UnityEvent OnArrival;
 
-    public void MovePlayerOffBoatToWorld()
-    {
-        Player.transform.parent = null;
+        public void MovePlayerOffBoatToWorld()
+        {
+            Player.transform.parent = null;
+            OnArrival.Invoke();
+        }
     }
 }
