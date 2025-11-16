0bfbd2e0ed7202422283dff7f13c6bd307340812
Halden <halden@theparkes.me.uk>
2025-11-16 11:32:33 +0000

    cutscene update

--- Assets/Scripts/Audio/StarterAnimationAudioPlayer.cs
+++ Assets/Scripts/Audio/StarterAnimationAudioPlayer.cs
@@ -4,6 +4,9 @@ public class StarterAnimationAudioPlayer : MonoBehaviour
 {
     public string music;
     public string writingSFX;
+    public string letterSFX;
+    public string splashSFX;
+    public string surfaceSFX;
 
     void OnWriting()
     {
@@ -13,5 +16,19 @@ public class StarterAnimationAudioPlayer : MonoBehaviour
     void StartMusic()
     {
         FMODUnity.RuntimeManager.PlayOneShot(music);
+    }
+
+    void LetterOpen()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(letterSFX);
+    }
+
+    void OnSplash()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(splashSFX);
+    }
+    void OnSurface()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(surfaceSFX);
     }
 }
