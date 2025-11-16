f871a396e620e14c1f32bc31eb9cf1cff41a4cf7
Halden <halden@theparkes.me.uk>
2025-11-16 10:11:47 +0000

    staff swing sfx

--- Assets/Scripts/PlayerAnimationSounds.cs
+++ Assets/Scripts/PlayerAnimationSounds.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class PlayerAnimationSounds : MonoBehaviour
 {
     public string stepSFX;
+    public string meleeSFX;
 
     void OnStep()
     {
         FMODUnity.RuntimeManager.PlayOneShot(stepSFX);
+    }
+
+    void OnMelee()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(meleeSFX);
     }
 }
