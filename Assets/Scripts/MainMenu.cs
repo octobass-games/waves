7ae19b74edfe855730ee07cb626851d52e392a66
adamskipadamski <adam.t.wright@outlook.com>
2025-11-16 20:42:00 +0000

    make controller work on MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void NewGame()
+    public GameObject InitiallySelectedButton;
+
+    public void NewGame()
     {
         SceneManager.LoadScene("OpeningScene");
+    }
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(InitiallySelectedButton);
     }
 }
