f38d46f2d3eca9b7f0c236fecf16e85c97088ccd
adamskipadamski <adam.t.wright@outlook.com>
2025-11-26 20:47:02 +0000

    add bank loading in StartGame scene

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
@@ -5,8 +8,48 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    [FMODUnity.BankRef]
+    public List<string> Banks;
+
+    private bool BanksLoaded;
+    private bool InputPressed;
+
     void Start()
     {
-        InputSystem.onAnyButtonPress.CallOnce(ctrl => SceneManager.LoadScene("MainMenu"));
+        InputSystem.onAnyButtonPress.CallOnce(ctrl =>
+        {
+            InputPressed = true;
+        });
+
+        foreach (string b in Banks)
+        {
+            FMODUnity.RuntimeManager.LoadBank(b, true);
+        }
+
+        /*
+            For Chrome / Safari browsers / WebGL.  Reset audio on response to user interaction (LoadBanks is called from a button press), to allow audio to be heard.
+        */
+        FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+        FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+
+        StartCoroutine(LoadBanks());
+    }
+
+    void Update()
+    {
+        if (BanksLoaded && InputPressed)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    IEnumerator LoadBanks()
+    {
+        while (!FMODUnity.RuntimeManager.HaveAllBanksLoaded)
+        {
+            yield return null;
+        }
+
+        BanksLoaded = true;
     }
 }
