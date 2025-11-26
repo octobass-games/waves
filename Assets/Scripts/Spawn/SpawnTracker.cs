7ef01d72e34bec2447aa73eb8c0e125aecdd2e74
adamskipadamski <adam.t.wright@outlook.com>
2025-11-26 09:57:35 +0000

    fix E1 east spawn

--- Assets/Scripts/Spawn/SpawnTracker.cs
+++ Assets/Scripts/Spawn/SpawnTracker.cs
@@ -56,6 +56,8 @@ namespace Octobass.Waves.Spawn
         public void SetSpawnPoint(SpawnPoint spawnPoint)
         {
             CurrentSpawnPoint = spawnPoint;
+
+            Debug.Log($"[SpawnPoint]: CurrentSpawnPoint set to {spawnPoint.Name}");
         }
 
         public void Load(SaveData saveData)
