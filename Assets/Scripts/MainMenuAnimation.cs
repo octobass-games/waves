2216de38fc7e403b049e67e2d45733e403c65e67
Megan <megan@theparkes.me.uk>
2025-11-26 10:39:37 +0000

    fix opening animation

---
+++ Assets/Scripts/MainMenuAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAnimation : MonoBehaviour
+{
+    public List<Animator> Buttons;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadButton(int button)
+    {
+        Buttons[button].SetTrigger("load");
+        Debug.Log("aspojidpasdipjasdjiop");
+    }
+}
