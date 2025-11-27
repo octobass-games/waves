334c55f7a3a8bff70276f5edec079ebafd935367
adamskipadamski <adam.t.wright@outlook.com>
2025-11-27 16:57:58 +0000

    add Projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,78 @@
+using Octobass.Waves.Attack;
+using UnityEngine;
+
+namespace Octobass.Waves.Attack
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody2D Body;
+
+        [SerializeField]
+        private Animator Animator;
+
+        [SerializeField]
+        private float Speed;
+
+        [SerializeField]
+        private LayerMask TargetLayerMask;
+
+        private Vector2 Direction = Vector2.zero;
+        private bool HasMadeImpact;
+        private ContactFilter2D TargetContactFilter;
+
+        private int ColliderCount;
+        private Collider2D[] Colliders = new Collider2D[3];
+
+        void Awake()
+        {
+            TargetContactFilter = new()
+            {
+                useLayerMask = true,
+                layerMask = TargetLayerMask
+            };
+        }
+
+        public void Init(Vector2 direction)
+        {
+            Direction = direction;
+        }
+
+        void Update()
+        {
+            if (HasMadeImpact)
+            {
+                Animator.SetTrigger("Explode");
+            }
+        }
+
+        void FixedUpdate()
+        {
+            ColliderCount = Body.Overlap(TargetContactFilter, Colliders);
+
+            if (ColliderCount > 0)
+            {
+                HasMadeImpact = true;
+
+                for (int i = 0; i < ColliderCount; i++)
+                {
+                    Collider2D collider = Colliders[i];
+
+                    if (collider.TryGetComponent(out IDamageable damageable))
+                    {
+                        damageable.OnOneShot();
+                    }
+                }
+            }
+            else
+            {
+                Body.MovePosition(Body.position + Direction * Speed * Time.fixedDeltaTime);
+            }
+        }
+
+        void OnExplosionAnimationEnd()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
