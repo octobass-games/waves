f7ee2034cd7d03678bbb87bef692a58c0ef1c5d3
Megan <megan@theparkes.me.uk>
2025-11-27 13:18:15 +0000

    TP UI

--- Assets/Scripts/Map/MapRoomDetailsRenderer.cs
+++ Assets/Scripts/Map/MapRoomDetailsRenderer.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapRoomDetailsRenderer : MonoBehaviour
 {
     public GameObject Player;
     public GameObject Waterporter;
     public GameObject Shell;
+    public GameObject SelectedBorder;
+
+    public void ResetAllOpacities()
+    {
+        Player.GetComponent<Image>().color = Color.white;
+        SelectedBorder.GetComponent<Image>().color = Color.white;
+        Waterporter.GetComponent<Image>().color = Color.white;
+        Shell.GetComponent<Image>().color = Color.white;
+    }
 }
