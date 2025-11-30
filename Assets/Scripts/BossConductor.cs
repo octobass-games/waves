be1bc87b6fa548326a66b44b0a261b70f7eab6ae
Megan <megan@theparkes.me.uk>
2025-11-30 10:34:32 +0000

    H7

--- Assets/Scripts/BossConductor.cs
+++ Assets/Scripts/BossConductor.cs
@@ -1,6 +1,13 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.U2D;
 
 public class BossConductor : MonoBehaviour
 {
+    public CinemachineCamera Camera;
+    public Transform Boss;
+   public void StartBattle()
+    {
+        Camera.Follow = Boss;
+    }
 }
